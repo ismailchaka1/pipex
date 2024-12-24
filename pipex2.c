c14d3ec69dcd0f500d3a7fdd7090dbe95c77c456
ismailchaka1 <78697709+ismailchaka1@users.noreply.github.com>
2024-12-24 20:34:46 +0100

    Working version of pipex

--- pipex2.c
+++ pipex2.c
@@ -1,60 +1,123 @@
-
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include "libft/libft.h"
+
+char **get_paths(char **env)
+{
+    int i = 0;
+    while (env[i])
+    {
+        if (strncmp(env[i], "PATH=", 5) == 0)
+            break;
+        i++;
+    }
+    return ft_split(env[i] + 5, ':');
+}
+
+char *get_command(char **paths, char *command)
+{
+    int i = 0;
+    char *path;
+    char *test;
+
+    while (paths[i])
+    {
+        test = ft_strjoin("/", command);
+        path = ft_strjoin(paths[i], test);  
+        free(test);
+
+        if (access(path, R_OK | X_OK) == 0)
+        {
+            return path;
+        }
+        free(path);
+        i++;
+    }
+    return NULL;
+}
 
 int main(int ac, char **av, char **envp)
 {
-    int fd[2];
-    int fd1 = open("in", O_RDONLY);
-    int fd2 = open("out", O_RDWR | O_TRUNC | O_CREAT);
-    if (fd1 == -1 || fd2 == -1)
-    {
-        return 1;
-    }
     if (ac < 5)
     {
         return 1;
     }
-    int i = 2;
-    if (pipe(fd) == -1) {
-        close(fd1);
-        close(fd2);
+
+    int fd[2];
+    int fd1 = open(av[1], O_RDONLY);
+    int fd2 = open(av[ac - 1], O_RDWR | O_TRUNC | O_CREAT, 0644);
+
+    if (fd1 == -1 || fd2 == -1)
+    {
+        perror("File error");
         return 1;
     }
+
+    int i = 2;
+    char **arguments;
+    char *executable;
+    char **paths = get_paths(envp);
+
+
+    dup2(fd1, STDIN_FILENO);
     while (i < ac - 1)
     {
-        // int pid = fork();
-        // if (pid == -1) {
-        //     close(fd1);
-        //     close(fd2);
-        //     return 1;
-        // }
-        // if (pid == 0)
-        // {       
-        if (i == 2)
+        if (pipe(fd) == -1)
         {
-            dup2(fd1, STDIN_FILENO);
-            dup2(fd[1], STDOUT_FILENO);
-            printf("first command %s\n", av[i]);
+            perror("Pipe error");
+            close(fd1);
+            close(fd2);
+            return 1;
         }
-        else if (i == ac - 2)
-        {
-            dup2(fd[0], STDIN_FILENO);
-            dup2(fd2, STDOUT_FILENO);
-            printf("last command %s\n", av[i]);
-        }
-        else
-        {
-            dup2(fd[0], STDIN_FILENO);
-            dup2(fd[1], STDOUT_FILENO);
-            printf("middle command %s\n", av[i]);
-        }
-        // }
-        i++;
-    }
 
+        arguments = ft_split(av[i], ' ');
+        executable = get_command(paths, arguments[0]);
+
+        if (executable == NULL)
+        {
+            fprintf(stderr, "Command not found: %s\n", arguments[0]);
+            free(executable);
+            free(arguments);
+            i++;
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+          
+            if (i == ac - 2)
+                dup2(fd2, STDOUT_FILENO);
+            else 
+                dup2(fd[1], STDOUT_FILENO); 
+
+            close(fd[0]);
+            close(fd[1]);
+            execve(executable, arguments, envp);
+            perror("Execve failed"); 
+            exit(0);
+        }
+        
+        free(executable);
+        for (int j = 0; arguments[j]; j++)
+            free(arguments[j]);
+        // free(paths);
+        free(arguments);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        wait(NULL);
+        i++; 
+    }
+    for (int j = 0; paths[j]; j++)
+        free(paths[j]);
+    free(paths);
+    close(fd1);
+    close(fd2);
+
+    return 0;
 }
